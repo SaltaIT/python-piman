13280ffa7537b151b3a339fff2e7db15564781c5
Jordi Prats <jordi.prats@gmail.com>
2019-01-23 17:42:18 +0100

    skel

---
+++ pfgen.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+"""
+Puppetfile generator
+"""
+
+import os
+import sys
+import json
+import argparse
+from github import Github
+from configparser import SafeConfigParser
+from distutils.version import LooseVersion
