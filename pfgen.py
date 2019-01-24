24eb5a13d2508ba41ea951bf86f414abdda9a0a1
Jordi Prats <jordi.prats@gmail.com>
2019-01-24 09:58:41 +0100

    base skel

--- pfgen.py
+++ pfgen.py
@@ -13,3 +13,26 @@ import argparse
 from github import Github
 from configparser import SafeConfigParser
 from distutils.version import LooseVersion
+
+if __name__ == '__main__':
+    try:
+        basedir = sys.argv[1]
+    except IndexError:
+        basedir = '.'
+
+    config = SafeConfigParser()
+    config.read(basedir+'/pfgen.config')
+
+    try:
+        GH_TOKEN = config.get('github', 'token').strip('"').strip()
+    except:
+        sys.exit("ERROR: PAT is mandatory")
+
+    try:
+        debug = config.getboolean('github', 'debug')
+    except:
+        debug=False
+
+    for section in config.sections():
+        if section!="github":
+            print(str(section))
